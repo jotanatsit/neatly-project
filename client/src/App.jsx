6f71c2f7efd69ec18984fcc2a0cbdc798c68cc8d
Jo-Tanatsit <jotanatsit@gmail.com>
2023-04-11 14:23:05 +0700

    feat: renamed homepage file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/Home";
+import HomePage from "./pages/Homepage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
-  );
+   return (
+      <Routes>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/register" element={<RegisterPage />} />
+         <Route path="/login" element={<LoginPage />} />
+      </Routes>
+   );
 }
 
 export default App;
