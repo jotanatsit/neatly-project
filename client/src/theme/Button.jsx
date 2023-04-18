4fa62dc519f37cbb40ceed16305e556c88ed058e
Jo-Tanatsit <jotanatsit@gmail.com>
2023-04-18 12:52:07 +0700

    config: config ghost button padding

--- client/src/theme/Button.jsx
+++ client/src/theme/Button.jsx
@@ -1,52 +1,69 @@
-import { defineStyleConfig } from '@chakra-ui/react';
+import { defineStyleConfig } from "@chakra-ui/react";
 import "@fontsource/open-sans";
 
 export const Button = defineStyleConfig({
-   baseStyle: {
-      fontFamily: 'Open Sans',
-      fontStyle: "normal",
-      fontWeight: "600",
-      lineHeight: "16px",
-      textAlign: "center",
-      borderRadius: "4px",
-   },
-   sizes: {
-      md: {
-         fontSize: "16px",
-         padding: "16px 32px"
-      }
-   },
-   variants: {
-      primary: {
-         color: "white",
-         bg: "orange.600",
-         _hover: { bg: 'orange.500', color: 'white' },
-         _focus: { bg: "orange.700", color: 'white' },
+  baseStyle: {
+    fontFamily: "Open Sans",
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "16px",
+    textAlign: "center",
+    borderRadius: "4px",
+  },
+  sizes: {
+    md: {
+      fontSize: "16px",
+      padding: "16px 32px",
+    },
+  },
+  variants: {
+    primary: {
+      color: "white",
+      bg: "orange.600",
+      _hover: { bg: "orange.500", color: "white" },
+      _focus: { bg: "orange.700", color: "white" },
+    },
+    secondary: {
+      color: "orange.500",
+      bg: "white",
+      border: "1px solid",
+      borderColor: "orange.500",
+      _hover: {
+        bg: "white",
+        color: "orange.400",
+        border: "1px solid",
+        borderColor: "orange.400",
       },
-      secondary: {
-         color: "orange.500",
-         bg: "white",
-         border: "1px solid",
-         borderColor: "orange.500",
-         _hover: { bg: 'white', color: 'orange.400', border: "1px solid", borderColor: "orange.400" },
-         _focus: { bg: "white", color: 'orange.600', border: "1px solid", borderColor: "orange.600" },
+      _focus: {
+        bg: "white",
+        color: "orange.600",
+        border: "1px solid",
+        borderColor: "orange.600",
       },
-      ghost: {
-         color: "orange.500",
-         _hover: { bg: "none", color: "orange.400" },
-         _focus: { bg: "none", color: "orange.400" },
+    },
+    ghost: {
+      padding: "4px 8px",
+      color: "orange.500",
+      _hover: { bg: "none", color: "orange.400" },
+      _focus: { bg: "none", color: "orange.400" },
+    },
+    disabledPrimary: {
+      _hover: "none",
+      _disabled: { bg: "gray.300", color: "gray.600" },
+    },
+    disabledSecondary: {
+      _hover: "none",
+      _disabled: {
+        bg: "gray.300",
+        color: "gray.400",
+        border: "1px solid",
+        borderColor: "gray.300",
       },
-      disabledPrimary: {
-         _hover: "none",
-         _disabled: { bg: "gray.300", color: 'gray.600' }
-      },
-      disabledSecondary: {
-         _hover: "none",
-         _disabled: { bg: "gray.300", color: 'gray.400', border: "1px solid", borderColor: "gray.300" }
-      },
-      disabledGhost: {
-         _hover: "none",
-         _disabled: { color: "gray.500" }
-      }
-   }
-})
+    },
+    disabledGhost: {
+      padding: "4px 8px",
+      _hover: "none",
+      _disabled: { color: "gray.500" },
+    },
+  },
+});
