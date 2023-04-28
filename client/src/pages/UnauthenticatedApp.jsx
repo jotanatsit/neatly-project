fca2952527dbf242927bfda8eebf988b1e720718
Peeranat Phengsalaeh <seed.peeranat@gmail.com>
2023-04-28 10:48:47 +0700

    feat: payment gateway set up waiting for data state passing

--- client/src/pages/UnauthenticatedApp.jsx
+++ client/src/pages/UnauthenticatedApp.jsx
@@ -4,7 +4,9 @@ import RegisterPage from "./RegisterPage";
 import LoginPage from "./LoginPage";
 import RoomDetailPage from "./RoomDetailPage";
 import BookingPage from "./BookingPage";
-import Card from "../Components/Card";
+// import Card from "../Components/Card";
+import Payment from "../Components/Payment";
+import Completion from "../Components/Completion";
 
 function UnauthenticatedApp() {
   return (
@@ -16,7 +18,9 @@ function UnauthenticatedApp() {
         <Route path="/roomdetail/:roomTypeId" element={<RoomDetailPage />} />
         <Route path="/booking" element={<BookingPage />} />
         <Route path="*" element={<LoginPage />} />
-        <Route path="/card" element={<Card />} />
+        {/* <Route path="/card" element={<Card />} /> */}
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/completion" element={<Completion />} />
       </Routes>
     </div>
   );
